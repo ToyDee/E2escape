160fdc5965e4b537b99c50ee2b4c1813ae4ed1b0
ToyDee <aungthuhtet31@gmail.com>
2026-06-24 09:55:45 +0700

    Initial commit

---
+++ Assets/3DGamekitLite/Scripts/Game/Core/InteractOnButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace Gamekit3D
+{
+    public class InteractOnButton : InteractOnTrigger
+    {
+
+        public string buttonName = "E";
+        public UnityEvent OnButtonPress;
+        public bool isClicked = false;
+
+        bool canExecuteButtons = false;
+
+        protected override void ExecuteOnEnter(Collider other)
+        {
+            canExecuteButtons = true;
+        }
+
+        protected override void ExecuteOnExit(Collider other)
+        {
+            canExecuteButtons = false;
+        }
+
+        void Update()
+        {
+            if (canExecuteButtons && Input.GetButtonDown(buttonName) && !isClicked)
+            {
+                OnButtonPress.Invoke();
+                isClicked = true;
+            }
+        }
+
+    } 
+}
